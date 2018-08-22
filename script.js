5fc0e0bdee953d52250c93a97c3cafc7e47088da
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:39:05 -0400

    Mobile view toggle script

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ $( document ).ready(function() {
 
 function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
-    document.getElementById("mobile-2").style.width = "360px";
+	document.getElementById("mobile-1").style.width = "360px";
+	document.getElementById("mobile-2").style.width = "360px";
+    document.getElementById("mobile-3").style.width = "360px";
   }
   else {
-    document.getElementById("mobile-2").style.width = "100%";
+	document.getElementById("mobile-1").style.width = "100%";
+	document.getElementById("mobile-2").style.width = "100%";
+    document.getElementById("mobile-3").style.width = "1000px";
   }
   
 }
