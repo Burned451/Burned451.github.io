07ea5b414a001fcd802687edb703a3770b2b8ebd
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:18:38 -0400

    Running navbar once page is ready

--- script.js
+++ script.js
@@ -3,4 +3,6 @@
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
 document.head.appendChild(jQueryScript);*/
 
+$( document ).ready(function() {
 $('#nav-bar').load('nav-bar.html');
+});
