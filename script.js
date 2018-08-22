908fad63a8accd825d44ad5eeb034fd1a1b2204e
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:36:38 -0400

    Mobile view toggle test

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
 	$('#nav-bar').load('nav-bar.html');
 });
+
+function toggle_mobile_view(obj) {
+  if($(obj).is(":checked")){
+    document.getElementById("mobile-2").style.width = "360px";
+  }
+  else {
+    document.getElementById("mobile-2").style.width = "100%";
+  }
+  
+}
