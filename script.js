61b5557fbb5f16c1c26886f8b6170e84bae8f9b3
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:23:45 -0400

    Loading jQuery 2nd attempt

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Include jQuery
-var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
-document.head.appendChild(jQueryScript);
+
 
 $( document ).ready(function() {
+	var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+	
 $('#nav-bar').load('nav-bar.html');
 });
