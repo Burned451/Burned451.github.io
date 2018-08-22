b348182deee7c93e1caf400d4f4e7142f242c167
Edward <eddyisme123@yahoo.ca>
2018-08-22 18:20:33 -0400

    Loading navbar from external js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
 
+$('#nav-bar').load('nav-bar.html');
