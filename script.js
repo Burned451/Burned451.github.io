65b325e06f6fcc6e0216e6f091b6b243a81dd092
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:25:08 -0400

    Reverting to jQuery in HTML file

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Include jQuery
-
-
 $( document ).ready(function() {
-	var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
-document.head.appendChild(jQueryScript);
-	
-$('#nav-bar').load('nav-bar.html');
+	$('#nav-bar').load('nav-bar.html');
 });
