37891d87ac619e45a2d1833d8305b741edde10b4
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:07:11 -0400

    Loaded jQuery first

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Include jQuery
-var jQueryScript = document.createElement('script');  
+/*var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
-document.head.appendChild(jQueryScript);
+document.head.appendChild(jQueryScript);*/
 
 $('#nav-bar').load('nav-bar.html');
