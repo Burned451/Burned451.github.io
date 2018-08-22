6545f7991aba9cc3fc9f6faaac645d38ad822a49
Edward <eddyisme123@yahoo.ca>
2018-08-22 19:54:08 -0400

    Finished mobile view switch

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ $( document ).ready(function() {
 function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
 	document.getElementById("mobile-1").style.width = "360px";
+	document.getElementById("mobile-1").style.left = "50%";
+	document.getElementById("mobile-1").style.transform = "translate-x(-50%)";
+	
 	document.getElementById("mobile-2").style.width = "360px";
+	document.getElementById("mobile-2").style.marginLeft = "auto";
+	document.getElementById("mobile-2").style.marginRight = "auto";
+	
     document.getElementById("mobile-3").style.width = "360px";
   }
   else {
 	document.getElementById("mobile-1").style.width = "100%";
+	document.getElementById("mobile-1").style.left = "0";
+	document.getElementById("mobile-1").style.transform = "none";
+	
 	document.getElementById("mobile-2").style.width = "100%";
+	document.getElementById("mobile-2").style.marginLeft = "0";
+	document.getElementById("mobile-2").style.marginRight = "0";
+	
     document.getElementById("mobile-3").style.width = "1000px";
   }
   
