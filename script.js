cb6101609547a4de43cd5e6e3b2ec615b5d2d1aa
Burned451 <eddyisme123@yahoo.ca>
2018-10-18 18:41:41 -0400

    Fixed nav bar sizing

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
 	document.getElementById("mobile-wrapper").style.width = "360px";
 	document.getElementById("mobile-wrapper").style.height = "640px";
+	document.getElementById("mobile-wrapper").style.top = "50%";
+	document.getElementById("mobile-wrapper").style.left = "50%";
+	document.getElementById("mobile-wrapper").style.transform = "translate(-50%, -50%)";
+		
+	document.getElementById("mobile-nav-bar").style.width = "342px";
+	document.getElementById("mobile-nav-bar").style.top = "calc(50% - 320px)";
   }
   else {
 	document.getElementById("mobile-wrapper").style.width = "100%";
 	document.getElementById("mobile-wrapper").style.height = "100%";
+	document.getElementById("mobile-wrapper").style.top = "0";
+	document.getElementById("mobile-wrapper").style.left = "0";
+	document.getElementById("mobile-wrapper").style.transform = "translate(0, 0)";
+	
+	document.getElementById("mobile-nav-bar").style.width = "100%";
+	document.getElementById("mobile-nav-bar").style.top = "0";
   }
   
 }
