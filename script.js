c7fe5c3e45e421a5e20b2c280de271c89f71ca31
Burned451 <eddyisme123@yahoo.ca>
2018-10-18 13:55:50 -0400

    Simplified mobile view

--- script.js
+++ script.js
@@ -4,26 +4,12 @@ $( document ).ready(function() {
 
 function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
-	document.getElementById("mobile-1").style.width = "360px";
-	document.getElementById("mobile-1").style.left = "50%";
-	document.getElementById("mobile-1").style.transform = "translateX(-50%)";
-	
-	document.getElementById("mobile-2").style.width = "360px";
-	document.getElementById("mobile-2").style.marginLeft = "auto";
-	document.getElementById("mobile-2").style.marginRight = "auto";
-	
-    document.getElementById("mobile-3").style.maxWidth = "360px";
+	document.getElementById("mobile-wrapper").style.width = "360px";
+	document.getElementById("mobile-wrapper").style.height = "640px";
   }
   else {
-	document.getElementById("mobile-1").style.width = "100%";
-	document.getElementById("mobile-1").style.left = "0";
-	document.getElementById("mobile-1").style.transform = "none";
-	
-	document.getElementById("mobile-2").style.width = "100%";
-	document.getElementById("mobile-2").style.marginLeft = "0";
-	document.getElementById("mobile-2").style.marginRight = "0";
-	
-    document.getElementById("mobile-3").style.maxWidth = "1000px";
+	document.getElementById("mobile-wrapper").style.width = "100%";
+	document.getElementById("mobile-wrapper").style.height = "100%";
   }
   
 }
