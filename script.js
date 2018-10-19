d4da419650f8828f1202a7424395d92f0c200b6c
Burned451 <eddyisme123@yahoo.ca>
2018-10-18 20:41:50 -0400

    Removed unused code

--- script.js
+++ script.js
@@ -11,18 +11,10 @@ function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
 	document.getElementById("mobile-wrapper").style.width = mobile_width + "px";
 	document.getElementById("mobile-wrapper").style.height = mobile_height + "px";
-		
-//	document.getElementById("mobile-nav-bar").style.width = "342px";
-//	document.getElementById("mobile-nav-bar").style.top = "calc(50% - 320px)";
-//	document.getElementById("mobile-nav-bar").style.left = "calc(50% - 180px)";
   }
   else {
 	document.getElementById("mobile-wrapper").style.width = "100%";
 	document.getElementById("mobile-wrapper").style.height = "100%";
-	
-//	document.getElementById("mobile-nav-bar").style.width = "100%";
-//	document.getElementById("mobile-nav-bar").style.top = "0";
-//	document.getElementById("mobile-nav-bar").style.left = "0";
   }
   
 }
