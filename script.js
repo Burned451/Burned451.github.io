91dc4ec6826d1059629789127dd4c7cc9f7af88f
Burned451 <eddyisme123@yahoo.ca>
2018-10-25 16:48:28 -0400

    Replaced unset height with initial

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function toggle_mobile_view(obj) {
   if($(obj).is(":checked")){
 	document.getElementById("mobile-wrapper").style.width = mobile_width + "px";
 	document.getElementById("mobile-wrapper").style.maxHeight = mobile_height + "px";
-	document.getElementById("mobile-wrapper").style.minHeight = "unset";
+	document.getElementById("mobile-wrapper").style.minHeight = "initial";
   }
   else {
 	document.getElementById("mobile-wrapper").style.width = "100%";
-	document.getElementById("mobile-wrapper").style.maxHeight = "unset";
+	document.getElementById("mobile-wrapper").style.maxHeight = "initial";
 	document.getElementById("mobile-wrapper").style.minHeight = "100%";
   }
   
