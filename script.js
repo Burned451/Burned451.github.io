26c883b1652043fe9bd1d8b1a746edb71ed37127
Burned451 <eddyisme123@yahoo.ca>
2018-12-27 14:43:27 -0500

    Loaded page when url entered

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
 $(document).ready(function() {
-	$("#dynamic-content").load("home.html");
+	load_page()
 	
 	//$(".dropdown-content a").click(function(){
 	$(window).on('hashchange', function() {		
-		$("#dynamic-content").empty();
-		switch(window.location.hash) {
-			case '#gallery':
-				$('title').html('Edward Boguslavsky - Gallery');
-				$('.header-title').html('Gallery');
-				$('.header-subtitle').html('Photographs I have taken during my travels and spare time');
-				$("#dynamic-content").load("gallery.html");
-				break;
-			case '#links':
-				$('title').html('Edward Boguslavsky - Links');
-				$('.header-title').html('Links');
-				$('.header-subtitle').html('Other enjoyable media to keep you busy');
-				$("#dynamic-content").load("links.html");
-				break;
-			case '#about':
-				$('title').html('Edward Boguslavsky - About');
-				$('.header-title').html('About');
-				$('.header-subtitle').html('Additional information about me and different ways to keep in touch');
-				$("#dynamic-content").load("about.html");
-				break;
-			default:
-				$('title').html('Edward Boguslavsky - Home');
-				$('.header-title').html('Home');
-				$('.header-subtitle').html('An introduction to the site\'s content');
-				$("#dynamic-content").load("home.html");
-		}
+		load_page()
 	});
 });
 
@@ -37,5 +12,34 @@ function toggle_mobile_view(obj) {
 		$("#mobile-wrapper").css({'width': '360px', 'height': '640px'});
 	} else {
 		$("#mobile-wrapper").css({'width': '', 'height': ''});
+	}
+}
+
+function load_page() {
+	$("#dynamic-content").empty();
+	switch(window.location.hash) {
+		case '#gallery':
+			$('title').html('Edward Boguslavsky - Gallery');
+			$('.header-title').html('Gallery');
+			$('.header-subtitle').html('Photographs I have taken during my travels and spare time');
+			$("#dynamic-content").load("gallery.html");
+			break;
+		case '#links':
+			$('title').html('Edward Boguslavsky - Links');
+			$('.header-title').html('Links');
+			$('.header-subtitle').html('Other enjoyable media to keep you busy');
+			$("#dynamic-content").load("links.html");
+			break;
+		case '#about':
+			$('title').html('Edward Boguslavsky - About');
+			$('.header-title').html('About');
+			$('.header-subtitle').html('Additional information about me and different ways to keep in touch');
+			$("#dynamic-content").load("about.html");
+			break;
+		default:
+			$('title').html('Edward Boguslavsky - Home');
+			$('.header-title').html('Home');
+			$('.header-subtitle').html('An introduction to the site\'s content');
+			$("#dynamic-content").load("home.html");
 	}
 }
