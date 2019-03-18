55656882e06626f5174988a7314e17be4cc12f35
Burned451 <eddyisme123@yahoo.ca>
2019-03-18 14:17:27 -0400

    Get scroll position of body instead

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 });
 
 $(document).scroll(function() {
-	if ($('.title').scrollTop() > 0) $('.header').css('background', 'rgba(0, 0, 0, 0.3)');
+	if ($('body').scrollTop() > 0) $('.header').css('background', 'rgba(0, 0, 0, 0.3)');
 	else $('.header').css('background', '');
 });
 
